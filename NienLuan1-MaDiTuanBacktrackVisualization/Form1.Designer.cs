9fb77a89c49fbd1c34a7ded4cd58f8154843ea4a
hzjanuary <nhhoang03012004@gmail.com>
2024-12-12 08:31:11 +0700

    Update source code

---
+++ NienLuan1-MaDiTuanBacktrackVisualization/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace NienLuan1_MaDiTuanBacktrackVisualization
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tblChessBoard = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tblChessBoard
+            // 
+            this.tblChessBoard.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.InsetDouble;
+            this.tblChessBoard.ColumnCount = 8;
+            this.tblChessBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tblChessBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tblChessBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tblChessBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tblChessBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tblChessBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tblChessBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tblChessBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tblChessBoard.Location = new System.Drawing.Point(56, 45);
+            this.tblChessBoard.Name = "tblChessBoard";
+            this.tblChessBoard.RowCount = 8;
+            this.tblChessBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tblChessBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tblChessBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tblChessBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tblChessBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tblChessBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tblChessBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tblChessBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tblChessBoard.Size = new System.Drawing.Size(480, 480);
+            this.tblChessBoard.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(615, 270);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập tọa độ X";
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(713, 267);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(90, 22);
+            this.txtX.TabIndex = 2;
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(713, 295);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(90, 22);
+            this.txtY.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(615, 298);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nhập tọa độ Y";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(656, 357);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(98, 54);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(609, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Bài toán Mã đi tuần";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(609, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Thuật toán : Quay lui";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(609, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(251, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Giảng viên phụ trách  : Nguyễn Chí Cường";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(609, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(198, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "227060096 - Nguyễn Huy Hoàng";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 547);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tblChessBoard);
+            this.Name = "MainForm";
+            this.Text = "Mã đi tuần trực quan hóa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tblChessBoard;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ NienLuan1-MaDiTuanBacktrackVisualization/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NienLuan1_MaDiTuanBacktrackVisualization
+{
+    public partial class MainForm : Form
+    {
+        const int kichthuoc = 8;
+        int[,] KhungCo = new int[kichthuoc + 1, kichthuoc + 1];
+        int[] x = { 2, 1, -1, -2, -2, -1, 1, 2 };
+        int[] y = { 1, 2, 2, 1, -1, -2, -2, -1 };
+        bool thang = false;
+        public MainForm()
+        {
+            InitializeComponent();
+            HienThiBanCo();
+        }
+        private void HienThiBanCo()
+        {
+            tblChessBoard.Controls.Clear();
+            tblChessBoard.RowCount = kichthuoc;
+            tblChessBoard.ColumnCount = kichthuoc;
+
+            for (int i = 0; i < kichthuoc; i++)
+            {
+                for (int j = 0; j < kichthuoc; j++)
+                {
+                    Label cell = new Label
+                    {
+                        Dock = DockStyle.Fill,
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        BackColor = (i + j) % 2 == 0 ? Color.White : Color.Black,
+                        ForeColor = Color.Black,
+                        Font = new Font("Arial", 12, FontStyle.Bold)
+                    };
+                    tblChessBoard.Controls.Add(cell, j, i);
+                }
+            }
+        }
+        private async Task Di(int x_m, int y_m, int n)
+        {
+            int x_t, y_t;
+            for (int i = 0; i < 8; i++)
+            {
+                x_t = x_m + x[i];
+                y_t = y_m + y[i];
+                if (0 < x_t && x_t <= kichthuoc && 0 < y_t && y_t <= kichthuoc && KhungCo[x_t, y_t] == 0)
+                {
+                    KhungCo[x_t, y_t] = n;
+                    MarkPosition(x_t - 1, y_t - 1, n);  // Đánh dấu ô bằng màu
+
+                    await Task.Delay(200);
+
+                    if (n == kichthuoc * kichthuoc)
+                    {
+                        thang = true;
+                        MessageBox.Show("Thành công!");
+                        return;
+                    }
+
+                    if (!thang)
+                    {
+                        await Di(x_t, y_t, n + 1);
+                    }
+
+                    if (thang) return;
+
+                    KhungCo[x_t, y_t] = 0;
+                    ClearPosition(x_t - 1, y_t - 1);  // Xóa màu khi quay lui
+                    await Task.Delay(100);
+                }
+            }
+        }
+        private void MarkPosition(int row, int col, int step)
+        {
+            var cell = tblChessBoard.GetControlFromPosition(col, row) as Label;
+            if (cell != null)
+            {
+                cell.Text = step.ToString();
+                cell.BackColor = Color.Purple;  // Màu cho các bước di chuyển
+            }
+        }
+        private void ClearPosition(int row, int col)
+        {
+            var cell = tblChessBoard.GetControlFromPosition(col, row) as Label;
+            if (cell != null)
+            {
+                cell.Text = "";
+                cell.BackColor = (row + col) % 2 == 0 ? Color.White : Color.Gray;
+            }
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            int x_chon = int.Parse(txtX.Text);
+            int y_chon = int.Parse(txtY.Text);
+
+            if (x_chon < 1 || x_chon > kichthuoc || y_chon < 1 || y_chon > kichthuoc)
+            {
+                MessageBox.Show("Tọa độ không hợp lệ!");
+                return;
+            }
+
+            for (int i = 0; i <= kichthuoc; i++)
+            {
+                for (int j = 0; j <= kichthuoc; j++)
+                {
+                    KhungCo[i, j] = 0;
+                }
+            }
+
+            KhungCo[x_chon, y_chon] = 1;
+            MarkPosition(y_chon - 1, x_chon - 1, 1);
+
+            thang = false;
+            await Di(x_chon, y_chon, 2);
+
+            if (!thang)
+            {
+                MessageBox.Show("Không có lời giải.");
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
